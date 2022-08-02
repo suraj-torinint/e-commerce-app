3cd949b96e998640a8deae312dd564fd799fa8ce
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-08-02 18:26:44 +0530

    added delete, cart, wishlist button

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -3,13 +3,18 @@ import React from "react";
 const Card = (props) => {
     return (
         <>
-            <div className="col-sm-4 scard p-5">
+            <div className="col-sm-4 p-5">
                 <div className="">
                     <div className="card">
-                        <div className="text-center pt-3">
+                        <div className="">
+                            <button type="button" className="btn btn-secondary">
+                                <i class="bi bi-x-lg"></i>
+                            </button>
+                        </div>
+                        <div className="text-center">
                             <img
                                 src={props.image}
-                                className="card-img-top"
+                                className="card-img-top img-thumbnail"
                                 style={{ width: "200px", height: "200px" }}
                                 alt="..."
                             />
@@ -19,6 +24,12 @@ const Card = (props) => {
                             <p className="card-text">{props.desc}</p>
                             <a href="#" className="btn btn-primary">
                                 Go somewhere
+                            </a>
+                            <a href="#" className="btn btn-default mx-3">
+                                <i className="bi bi-cart"></i>
+                            </a>
+                            <a href="#" className="btn btn-default">
+                                <i class="bi bi-heart-fill danger"></i>
                             </a>
                         </div>
                     </div>
