c1f7ff734428af3ac6505d7c87146d9a60cd62dd
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-08-04 13:14:30 +0530

    added feature of cross button to delete the card

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      {/* <p>Hello World!</p> */}
     </div>
   );
 }
--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,43 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = (props) => {
+    const [isActive, setIsActive] = useState("col-sm-4 p-5");
+
+    const handleOnClick = () => {
+        console.log("delete clicked");
+        setIsActive("d-none");
+    };
+
     return (
         <>
-            <div className="col-sm-4 p-5">
-                <div className="">
-                    <div className="card">
-                        <div className="">
-                            <button type="button" className="btn btn-secondary">
-                                <i class="bi bi-x-lg"></i>
-                            </button>
-                        </div>
-                        <div className="text-center">
-                            <img
-                                src={props.image}
-                                className="card-img-top img-thumbnail"
-                                style={{ width: "200px", height: "200px" }}
-                                alt="..."
-                            />
-                        </div>
-                        <div className="card-body">
-                            <h5 className="card-title">{props.title}</h5>
-                            <p className="card-text">{props.desc}</p>
-                            <a href="#" className="btn btn-primary">
-                                Show more
-                            </a>
-                            <a href="#" className="btn btn-default mx-3">
-                                <i className="bi bi-cart"></i>
-                            </a>
-                            <a href="#" className="btn btn-default">
-                                <i class="bi bi-heart-fill danger"></i>
-                            </a>
-                            <a href="#" className="btn btn-default mx-3">
-                                <i className="bi bi-cart"></i>
-                            </a>
-                            <a href="#" className="btn btn-default">
-                                <i class="bi bi-heart-fill danger"></i>
-                            </a>
-                        </div>
+            <div className={isActive}>
+                <div className="card">
+                    <div className="">
+                        <button onClick={handleOnClick} type="button" className="btn btn-secondary">
+                            <i className="bi bi-x-lg"></i>
+                        </button>
+                    </div>
+                    <div className="text-center">
+                        <img src={props.image} className="card-img-top img-thumbnail" style={{ width: "200px", height: "200px" }} alt="..." />
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.desc}</p>
+                        <a href="#" className="btn btn-primary">
+                            Show more
+                        </a>
+                        <a href="#" className="btn btn-default mx-3">
+                            <i className="bi bi-cart"></i>
+                        </a>
+                        <a href="#" className="btn btn-default">
+                            <i className="bi bi-heart"></i>
+                        </a>
                     </div>
                 </div>
             </div>
