8e602ab45800a1278f489b2f5c17db9af41b4560
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-08-05 10:58:40 +0530

    moved components to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Card from "./Component/Card";
+import productData from "./productData";
+
 function App() {
-  return (
-    <div className="App">
-      <p>Hello World!</p>
-    </div>
-  );
+    return (
+        <div className="row">
+            <Card image={productData[0].image} title={productData[0].title} desc={productData[0].desc} />
+            <Card image={productData[1].image} title={productData[1].title} desc={productData[1].desc} />
+            <Card image={productData[2].image} title={productData[2].title} desc={productData[2].desc} />
+            <Card image={productData[3].image} title={productData[3].title} desc={productData[3].desc} />
+            <Card image={productData[4].image} title={productData[4].title} desc={productData[4].desc} />
+        </div>
+    );
 }
 
 export default App;
