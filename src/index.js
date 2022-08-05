e46103278bbf11127be8bab85c4813a37f0e9982
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-08-05 10:55:46 +0530

    removed multiple compnents to App.js & div

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Header from "./Component/Header";
-import Card from "./Component/Card";
-import productData from "./productData";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Header />
         <div className="container">
-            <div className="row">
-                <Card image={productData[0].image} title={productData[0].title} desc={productData[0].desc} />
-                <Card image={productData[1].image} title={productData[1].title} desc={productData[1].desc} />
-                <Card image={productData[2].image} title={productData[2].title} desc={productData[2].desc} />
-                <Card image={productData[3].image} title={productData[3].title} desc={productData[3].desc} />
-                <Card image={productData[4].image} title={productData[4].title} desc={productData[4].desc} />
-                <App />
-            </div>
+            <App />
         </div>
     </React.StrictMode>
 );
