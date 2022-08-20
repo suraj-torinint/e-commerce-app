871df67f174ac6f2283d081ec1ca4c356645854f
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-08-20 17:28:29 +0530

    Done with the conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Card from "./Component/Card";
-import productData from "./productData";
 import Header from "./Component/Header";
 import AddItem from "./Component/Additem";
+import productData from "./productData";
 
 function App() {
     const [cartItem, setcartItem] = useState();
@@ -14,9 +14,7 @@ function App() {
             <Header cartItem={cartItem} />
             <div className="container">
                 <div className="row">
-                    {productData.map((data) => (
-                        <Card func={func} key={data.id} image={data.image} title={data.title} desc={data.desc} price={data.price} width={data.width} height={data.height} />
-                    ))}
+                    {productData.length !== 0 ? productData.map((data) => <Card func={func} key={data.id} image={data.image} title={data.title} desc={data.desc} price={data.price} width={data.width} height={data.height} />) : <h1>No Data Found</h1>}
                 </div>
                 <AddItem />
             </div>
